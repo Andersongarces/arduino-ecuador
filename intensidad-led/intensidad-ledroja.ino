1e579315b1b276fff1b41d94639eed166e5015bc
Andersongarces <146659066+Andersongarces@users.noreply.github.com>
2024-02-15 20:30:44 -0500

    Add files via upload

---
+++ intensidad-led/intensidad-ledroja.ino
@@ -0,0 +1,15 @@
+const int red = 10;
+const int dt = 500;
+const int brightness[] = {10, 100, 200, 255};
+const int numBrightness = sizeof(brightness) / sizeof(brightness[0]);
+
+void setup() {
+  pinMode(red, OUTPUT); 
+}
+
+void loop() {
+  for (int i = 0; i < numBrightness; ++i) {
+    analogWrite(red, brightness[i]);
+    delay(dt);
+  }
+}
